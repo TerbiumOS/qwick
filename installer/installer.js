ba407017ca8b978acc8e04b1e2e692522b183cf9
Alex <166651652+ironswordX@users.noreply.github.com>
2025-07-15 17:38:06 -0400

    base

--- installer/installer.js
+++ installer/installer.js
@@ -3,4 +3,15 @@ var Lockfile = JSON.parse(await Filer.fs.promises.readFile("/system/qwick/Lockfi
 var filesRoot = "https://raw.githubusercontent.com/TerbiumOS/qwick/refs/heads/main/qwick";
 var qwickRoot = Metafile.root;
 
-var filetableRaw = tb.libcurl.fetch("https://raw.githubusercontent.com/TerbiumOS/qwick/refs/heads/main/qwick")
+var filetableRaw = await tb.libcurl.fetch(`https://raw.githubusercontent.com/TerbiumOS/qwick/refs/heads/main/installer/filetable.json?ts=${Date.now()}`);
+var filetable = await filetableRaw.json();
+
+var filetableKeys = Object.keys(filetable);
+
+for (let i = 0; i < filetableKeys.length; i++) {
+    let src = filetableKeys[i];
+    let dest = filetable[filetableKeys[i]].replace("$ROOT", qwickRoot);
+    let raw = await tb.libcurl.fetch(`${filesRoot}${src}?ts=${Date.now()}`);
+    let content = await raw.text();
+    await Filer.fs.promises.writeFile(dest, content);
+}
