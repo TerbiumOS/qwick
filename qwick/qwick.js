ae0ee29d4ba2003754c9ba070a7e1d2e7417ec01
Alex <166651652+ironswordX@users.noreply.github.com>
2025-07-15 19:04:53 -0400

    fix update cmd

--- qwick/qwick.js
+++ qwick/qwick.js
@@ -76,11 +76,12 @@ async function qwick(args) {
 		case "help":
 			help(args._);
 			break;
-        case "update":
-            var version = (JSON.parse(await Filer.fs.promises.readFile("/system/qwick/Metafile", "utf8"))).version;
+        case "update": {
+            var Metafile = JSON.parse(await Filer.fs.promises.readFile("/system/qwick/Metafile", "utf8"));
+            var version = Metafile.version;
             var remoteVersionRaw = await terbium.libcurl.fetch(`https://raw.githubusercontent.com/TerbiumOS/qwick/refs/heads/main/version?ts=${Date.now()}`);
-            var remoteVersion = await remoteVersionRaw.text();
-            if (version != remoteVersion) {
+            var remoteVersion = (await remoteVersionRaw.text()).trim();
+            if (version.trim() != remoteVersion) {
                 displayOutput("Updating Qwick...");
                 const installerRaw = await terbium.libcurl.fetch(`https://raw.githubusercontent.com/TerbiumOS/qwick/refs/heads/main/installer/installer.js?ts=${Date.now()}`);
                 const installerBody = await installerRaw.text();
@@ -89,11 +90,16 @@ async function qwick(args) {
                 })`;
                 const installerFn = eval(wrappedInstallerBody);
                 await installerFn(terbium, Filer, displayOutput, displayError);
-                displayOutput("Finished updating Qwick");
+                displayOutput("Writing update to Metafile");
+                Metafile.version = remoteVersion;
+                await Filer.fs.promises.writeFile("/system/qwick/Metafile", JSON.stringify(Metafile, null, 2));
+                displayOutput(`Finished updating Qwick to v${remoteVersion}`);
             } else {
                 displayOutput("No new qwick version available");
             }
             createNewCommandInput();
+            break;
+        }
 		default:
 			error(`qwick > unknown subcommand: ${args._[0]}`);
 			break;
