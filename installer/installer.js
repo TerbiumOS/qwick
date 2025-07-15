e0ac0cccd7f0258d9948946a7ee338639a0dd165
Alex <166651652+ironswordX@users.noreply.github.com>
2025-07-15 17:02:04 -0400

    oops

--- installer/installer.js
+++ installer/installer.js
@@ -1,2 +1,2 @@
-var test = await Filer.fs.promises.readFile("/system/qwick/Metafile");
+var test = await Filer.fs.promises.readFile("/system/qwick/Metafile", "utf8");
 displayOutput(JSON.stringify(test, null, 2));
